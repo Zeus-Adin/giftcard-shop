c4df31c78cad82e537c09f6e08b0f9f32cc91bcb
Zeus-Adin <bollyseaglesway1@gmail.com>
2023-09-24 12:51:22 +0100

    updates

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,16 +1,13 @@
-import { useEffect, useState } from "react";
 import Home from "./page/home/Home";
 import Landing from "./page/landing/Landing";
-import { user } from "./services/user";
-
 import './routes.css'
 import Login from "./page/auth/login/Index";
 
 const Routes = ({ path, redirect, authed }) => {
     return (
         <div className="main">
-            {path === "/" && <Home redirect={redirect} authed={authed} />}
-            {path === "/landing" && <Landing redirect={redirect} />}
+            {path === "/" && <Landing redirect={redirect} />}
+            {path === "/dashboard" && <Home redirect={redirect} authed={authed} />}
             {path === "/login" && <Login redirect={redirect} />}
             {path === "/register" && <Login redirect={redirect} />}
         </div>
