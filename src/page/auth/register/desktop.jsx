18daf069e28a79a221252a0ad02ef477e26dae67
Zeus-Adin <bollyseaglesway1@gmail.com>
2023-09-27 23:31:17 +0100

    updates

--- src/page/auth/register/desktop.jsx
+++ src/page/auth/register/desktop.jsx
@@ -59,7 +59,7 @@ const FormTittle = styled('p')(({ }) => ({
     color: '#3F1E4B',
     textAlign: 'center',
     lineHeight: '0.5rem',
-    fontSize: '2rem',
+    fontSize: 'rem',
     fontWeight: 'bold',
     letterSpacing: '0.2px',
     textDecoration: 'none',
@@ -67,7 +67,7 @@ const FormTittle = styled('p')(({ }) => ({
     marginBlockEnd: '1em',
     marginInlineStart: '0px',
     marginInlineEnd: '0px',
-    marginBottom: '2rem',
+    marginBottom: '1rem',
 }))
 const InputsWrap = styled('div')(({ }) => ({
     width: '100%',
@@ -98,7 +98,7 @@ const MobileInputWrapper = styled('div')(({ }) => ({
 }))
 const MobileSubText = styled('span')(({ }) => ({
     marginTop: "0.2rem",
-    fontFamily: "CeraPro-Medium, -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+    fontFamily: "--fonts-medium",
     fontSize: "1.2rem",
     justifyContent: 'center',
     textJustify: 'center',
@@ -120,17 +120,6 @@ const FooterWrapper = styled('div')(({ }) => ({
     display: 'flex',
     marginTop: '1rem'
 }))
-const ForgotButton = styled('button')(({ }) => ({
-    cursor: 'pointer',
-    fontFamily: "CeraPro-Medium,-apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
-    textAlign: 'center',
-    lineHeight: "2.4rem",
-    color: "#7165E3",
-    letterSpacing: '0.2px',
-    textDecoration: 'none',
-    fontSize: '1.2rem',
-    marginBottom: '1rem'
-}))
 const SubmitBtn = styled('button')(({ }) => ({
     backgroundColor: 'yellow',
     all: 'unset',
@@ -144,7 +133,7 @@ const SubmitBtn = styled('button')(({ }) => ({
     fontFamily: '--fonts-medium',
     whiteSpaceCollapse: 'collapse',
     textWrap: 'nowrap',
-    gap: '--space-3',
+    // gap: '--space-3',
     transition: 'all 0.2s ease-in-out 0s',
 }))
 const SubmitBtnText = styled('div')(({ }) => ({
@@ -155,7 +144,32 @@ const SubmitBtnText = styled('div')(({ }) => ({
     fontFamily: "--fonts-bold",
     fontSize: "medium"
 }))
-
+const FooterText = styled('p')(({ }) => ({
+    marginTop: '0rem',
+    fontFamily: "--fonts-medium",
+    fontWeight: '400',
+    color: '#4F4F4F',
+    fontSize: '1.2rem',
+    textAlign: 'center',
+    lineHeight: '2.4rem',
+    letterSpacing: ' 0.2px',
+    textDecoration: 'none',
+    display: 'block',
+    marginBlockStart: '1em',
+    marginBlockEnd: '1em',
+    marginInlineStart: '0px',
+    marginInlineEnd: '0px'
+}))
+const FooterBtn = styled('button')(({ }) => ({
+    cursor: 'pointer',
+    fontFamily: "CeraPro-Medium, -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+    textAlign: 'center',
+    lineHeight: '2.4rem',
+    color: '#7165E3',
+    letterSpacing: '0.2px',
+    textDecoration: 'none',
+    fontSize: '1.2rem'
+}))
 const DesktopView = () => {
     const [timerValue, setTimerValue] = useState(1);
     // Function for image slides
@@ -184,13 +198,25 @@ const DesktopView = () => {
                             <FormTittle>Create Account</FormTittle>
                             <MobileInputWrapper>
                                 <FormControl sx={{ width: '90%' }}>
-                                    <FormLabel>Email address</FormLabel>
+                                    <FormLabel>Username</FormLabel>
                                     <InputBase
                                         fullWidth
                                         required
-                                        id="email"
-                                        label="Email address"
-                                        placeholder="Email..."
+                                        id="username"
+                                        label="Username"
+                                        placeholder="e.g Philip"
+                                    />
+                                </FormControl>
+                            </MobileInputWrapper>
+                            <MobileInputWrapper>
+                                <FormControl sx={{ width: '90%' }}>
+                                    <FormLabel>Phone number</FormLabel>
+                                    <InputBase
+                                        fullWidth
+                                        required
+                                        id="phone"
+                                        label="Phone number"
+                                        placeholder="e.g 090 6654 2214"
                                     />
                                 </FormControl>
                             </MobileInputWrapper>
@@ -202,31 +228,7 @@ const DesktopView = () => {
                                         required
                                         id="email"
                                         label="Email address"
-                                        placeholder="Email..."
-                                    />
-                                </FormControl>
-                            </MobileInputWrapper>
-                            <MobileInputWrapper>
-                                <FormControl sx={{ width: '90%' }}>
-                                    <FormLabel>Email address</FormLabel>
-                                    <InputBase
-                                        fullWidth
-                                        required
-                                        id="email"
-                                        label="Email address"
-                                        placeholder="Email..."
-                                    />
-                                </FormControl>
-                            </MobileInputWrapper>
-                            <MobileInputWrapper>
-                                <FormControl sx={{ width: '90%' }}>
-                                    <FormLabel>Email address</FormLabel>
-                                    <InputBase
-                                        fullWidth
-                                        required
-                                        id="email"
-                                        label="Email address"
-                                        placeholder="Email..."
+                                        placeholder="e.g philip@example.com"
                                     />
                                 </FormControl>
                             </MobileInputWrapper>
@@ -236,7 +238,7 @@ const DesktopView = () => {
                                     <InputBase
                                         required
                                         id="password"
-                                        placeholder="Password"
+                                        placeholder="****"
                                         type="password"
                                         autoComplete="current-password"
                                     />
@@ -245,13 +247,13 @@ const DesktopView = () => {
                             </MobileInputWrapper>
                             <MobileSubText>Password should be minimum of 8 Characters</MobileSubText>
                             <FooterWrapper>
-                                <ForgotButton>Forgot Password</ForgotButton>
                                 <SubmitBtn>
                                     <svg width="239" height="68" viewBox="0 0 239 68" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M0 33.6444C0 15.0631 15.0631 0 33.6444 0H207.171C224.749 0 239 14.2505 239 31.8295V31.8295C239 49.1489 225.152 63.2897 207.836 63.652L34.3481 67.2814C15.4955 67.6758 0 52.5011 0 33.6444V33.6444Z" fill="#F5CF48"></path>
                                     </svg>
-                                    <SubmitBtnText>Login</SubmitBtnText>
+                                    <SubmitBtnText>Create Account</SubmitBtnText>
                                 </SubmitBtn>
+                                <FooterText color="black">Already have an account? <FooterBtn>Login</FooterBtn></FooterText>
                             </FooterWrapper>
                         </Grid>
 
