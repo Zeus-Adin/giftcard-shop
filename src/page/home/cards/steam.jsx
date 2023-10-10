ae6cf212a1d7843c35ac87d6fc1b2182524f25ad
Zeus-Adin <bollyseaglesway1@gmail.com>
2023-10-10 13:38:34 +0100

    Updates

--- src/page/home/cards/steam.jsx
+++ src/page/home/cards/steam.jsx
@@ -254,9 +254,9 @@ const ReturnCalculatorHeaderText = styled('p')(({ }) => ({
     lineHeight: '1.6rem',
     letterSpacing: '0.2px',
     textDecoration: 'none',
-    marginTop:'1rem',
-    marginBottom:'1rem',
-    fontWeight:'500'
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    fontWeight: '500'
 }))
 const ReturnCalculatorValueWrapper = styled('div')(({ }) => ({
     gap: '0.8rem',
@@ -274,7 +274,36 @@ const ReturnCalculatorValueText = styled('p')(({ }) => ({
     fontSize: '2.8rem',
     letterSpacing: '0.2px',
     textDecoration: 'none',
-    fontWeight:'700'
+    fontWeight: '700'
+}))
+
+const ProceedBtnWrapper = styled('div')(({ }) => ({
+    flexWrap: 'nowrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    display: 'flex',
+}))
+const ProceedBtn = styled('button')(({ }) => ({
+    all: 'unset',
+    position: 'relative',
+    display: 'flex',
+    flexShrink: '0',
+    flexGrow: '0',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    fontFamily: "CeraPro-Medium, -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'",
+    whiteSpaceCollapse: 'collapse',
+    textWrap: 'nowrap',
+    gap: '1.6rem',
+    transition: 'all 0.2s ease-in-out 0s'
+}))
+const ProceedBtnText = styled('div')(({ }) => ({
+    position: 'absolute',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '1.6rem',
 }))
 // MovileView Components
 // const
@@ -297,7 +326,7 @@ const Steam = ({ redirect }) => {
             <Grid xs={{ sm: 'flex', xs: 'flex', lg: 'none', xl: 'none' }}>
                 <AppHeader>
                     <AppHeaderButtonWrapper>
-                        <AppHeaderButton onClick={()=>redirect('/dashboard')}>
+                        <AppHeaderButton onClick={() => redirect('/dashboard')}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M14.9993 20.67C14.8093 20.67 14.6193 20.6 14.4693 20.45L7.9493 13.93C6.8893 12.87 6.8893 11.13 7.9493 10.07L14.4693 3.55002C14.7593 3.26002 15.2393 3.26002 15.5293 3.55002C15.8193 3.84002 15.8193 4.32002 15.5293 4.61002L9.0093 11.13C8.5293 11.61 8.5293 12.39 9.0093 12.87L15.5293 19.39C15.8193 19.68 15.8193 20.16 15.5293 20.45C15.3793 20.59 15.1893 20.67 14.9993 20.67Z" color="#292D32" fill="black">
                                 </path>
@@ -396,6 +425,20 @@ const Steam = ({ redirect }) => {
                                     </ReturnCalculatorValueWrapper>
                                     <ReturnCalculatorHeaderText>Rate:0</ReturnCalculatorHeaderText>
                                 </ReturnCalculator>
+
+                                {/* ---------------------- */}
+
+                                <ProceedBtnWrapper>
+                                    <ProceedBtn>
+                                        <svg width="239" height="68" viewBox="0 0 239 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M0 33.6444C0 15.0631 15.0631 0 33.6444 0H207.171C224.749 0 239 14.2505 239 31.8295V31.8295C239 49.1489 225.152 63.2897 207.836 63.652L34.3481 67.2814C15.4955 67.6758 0 52.5011 0 33.6444V33.6444Z" fill="#F5CF48">
+                                            </path>
+                                        </svg>
+                                        <ProceedBtnText>Proceed</ProceedBtnText>
+                                    </ProceedBtn>
+                                </ProceedBtnWrapper>
+
+
 
                             </AppBodyContentsWrapper>
                         </AppBodyContents>
