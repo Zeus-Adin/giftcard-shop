75191ddf8c59870da4a2b846dc38142244e20495
Zeus-Adin <bollyseaglesway1@gmail.com>
2023-11-07 22:27:03 +0100

    update

--- src/page/auth/login/Index.jsx
+++ src/page/auth/login/Index.jsx
@@ -7,26 +7,15 @@ import {
     MobileHeaderBackButtonContent,
     MobileHeaderLogo,
     // Content Components
-    Content,
-    ContentWrapper,
-    ContentWrap,
-    FormWrapper,
-    FormHeaderText,
-    FormInputWrapper,
-    FormInputBoxWrapper,
-    FormInputBoxWrap,
-    FormInputBox,
-    FormInputLabel,
-    FormInput,
-    FormInputButton,
-    FormInputSubtext,
-    FormSubmitButtonWrapper,
-    FormForgotButton,
-    FormSubmitButton,
-    FormSubmitButtonText,
-    FormSubmitButtonSubText,
-    FormLoginButton
+    Content, ContentWrapper, ContentWrap, FormWrapper, FormHeaderText, FormInputWrapper, FormInputBoxWrapper,
+    FormInputBoxWrap, FormInputBox, FormInputLabel, FormInput, FormInputButton, FormInputSubtext, FormSubmitButtonWrapper, FormForgotButton,
+    FormSubmitButton, FormSubmitButtonText, FormSubmitButtonSubText, FormLoginButton
 } from "./components"
+import {
+    DesktopWrapper, DesktopWrap, DesktopContentWrapper, DesktopContentWrap, DesktopContent, DesktopSlideImage, DesktopContentInputArea,
+    DesktopHeaderText, FormContentWrapper, FormInputWrap, FormInputContent, FormToggleButton, FormSubtext, FormButtonWrapper, FormButton,
+    FormButtonText, FormButtonSubText, FormLoginSubTextButton, DesktopFloatImage
+} from '../register/components'
 import { useEffect, useState } from "react";
 import { login } from "./functions";
 import Cookies from "js-cookie";
@@ -39,11 +28,13 @@ const AppGrid = styled('div')(({ }) => ({
 
 const appOrigin = window.location.origin;
 const Login = ({ redirect, handleOpenAlertBox, setAlertText }) => {
+    const [timerValue, setTimerValue] = useState(1);
     const [passwordType, setPasswordType] = useState('password');
     const [emailValue, setEmailValue] = useState('');
     const [passwordValue, setPasswordValue] = useState('');
     const [submitBtn, setSubmitBtn] = useState(true);
     const [loading, setLoading] = useState(false);
+
     function handleTextChange(e) {
         const { name, value } = e.target;
         switch (name) {
@@ -91,6 +82,14 @@ const Login = ({ redirect, handleOpenAlertBox, setAlertText }) => {
         if (session) redirect('/dashboard');
     }, [])
 
+    useEffect(() => {
+        const updateTimer = () => {
+            setTimerValue((prevValue) => (prevValue % 5) + 1);
+        };
+        const intervalId = setInterval(updateTimer, 5000);
+        return () => clearInterval(intervalId);
+    }, [])
+
     return (
         <AppGrid  >
             {/* Mobile View */}
@@ -99,7 +98,7 @@ const Login = ({ redirect, handleOpenAlertBox, setAlertText }) => {
                 <MobileHeaderWrapper>
                     {/* Back Button */}
                     <MobileHeaderBackButtonWrapper>
-                        <MobileHeaderBackButtonContent onClick={()=>redirect('/')}>
+                        <MobileHeaderBackButtonContent onClick={() => redirect('/')}>
                             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M14.9993 20.67C14.8093 20.67 14.6193 20.6 14.4693 20.45L7.9493 13.93C6.8893 12.87 6.8893 11.13 7.9493 10.07L14.4693 3.55002C14.7593 3.26002 15.2393 3.26002 15.5293 3.55002C15.8193 3.84002 15.8193 4.32002 15.5293 4.61002L9.0093 11.13C8.5293 11.61 8.5293 12.39 9.0093 12.87L15.5293 19.39C15.8193 19.68 15.8193 20.16 15.5293 20.45C15.3793 20.59 15.1893 20.67 14.9993 20.67Z" color="#292D32" fill="black">
                                 </path>
@@ -163,7 +162,68 @@ const Login = ({ redirect, handleOpenAlertBox, setAlertText }) => {
 
             {/* Desktop View */}
             <Grid sx={{ display: { xs: 'none', sm: 'none', lg: 'flex', xl: 'flex' } }}>
-                Desktop
+                <DesktopWrapper>
+                    <DesktopWrap>
+                        <DesktopContentWrapper>
+                            <DesktopContentWrap>
+
+                                {/* Contents */}
+                                <DesktopContent>
+                                    <DesktopSlideImage src={`/svg/splash-${timerValue}.svg`} />
+                                </DesktopContent>
+                                <DesktopContentInputArea>
+                                    <DesktopHeaderText>Welcome back!</DesktopHeaderText>
+
+                                    <FormContentWrapper>
+
+                                        {/* Input Box */}
+                                        <FormInputWrapper>
+                                            <FormInputWrap>
+                                                <FormInputContent>
+                                                    <FormInputLabel>Email address</FormInputLabel>
+                                                    <FormInputBox placeholder="e.g you@email.com" name="email" required onBlur={handleTextChange} />
+                                                </FormInputContent>
+                                            </FormInputWrap>
+                                        </FormInputWrapper>
+
+                                        <FormInputWrapper>
+                                            <FormInputWrap>
+                                                <FormInputContent>
+                                                    <FormInputLabel>Password</FormInputLabel>
+                                                    <FormInputBox placeholder="e.g johndoe" type="password" name="pwd" required onBlur={handleTextChange} />
+                                                </FormInputContent>
+                                                <FormToggleButton>
+                                                    <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" color="#7165E3" height="22" width="22" xmlns="http://www.w3.org/2000/svg" >
+                                                        <path d="M396 512a112 112 0 1 0 224 0 112 112 0 1 0-224 0zm546.2-25.8C847.4 286.5 704.1 186 512 186c-192.2 0-335.4 100.5-430.2 300.3a60.3 60.3 0 0 0 0 51.5C176.6 737.5 319.9 838 512 838c192.2 0 335.4-100.5 430.2-300.3 7.7-16.2 7.7-35 0-51.5zM508 688c-97.2 0-176-78.8-176-176s78.8-176 176-176 176 78.8 176 176-78.8 176-176 176z">
+                                                        </path>
+                                                    </svg>
+                                                </FormToggleButton>
+                                            </FormInputWrap>
+                                        </FormInputWrapper>
+
+                                        <FormSubtext>Password should be minimum of 8 Characters</FormSubtext>
+
+                                    </FormContentWrapper>
+
+                                    <FormButtonWrapper>
+
+                                        <FormButton disabled={submitBtn} onClick={authenticate}>
+                                            <svg width="239" height="68" viewBox="0 0 239 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M0 33.6444C0 15.0631 15.0631 0 33.6444 0H207.171C224.749 0 239 14.2505 239 31.8295V31.8295C239 49.1489 225.152 63.2897 207.836 63.652L34.3481 67.2814C15.4955 67.6758 0 52.5011 0 33.6444V33.6444Z" fill="#F5CF48">
+                                                </path>
+                                            </svg>
+                                            <FormButtonText>{loading ? <CircularProgress /> : 'Submit'}</FormButtonText>
+                                        </FormButton>
+
+                                        <FormButtonSubText>Don't have an account? <FormLoginSubTextButton onClick={() => redirect('/register')}>Create an account</FormLoginSubTextButton></FormButtonSubText>
+                                    </FormButtonWrapper>
+                                </DesktopContentInputArea>
+
+                            </DesktopContentWrap>
+                        </DesktopContentWrapper>
+                        <DesktopFloatImage src="svg/auth-bird.svg" />
+                    </DesktopWrap>
+                </DesktopWrapper>
             </Grid>
         </AppGrid >
     )
